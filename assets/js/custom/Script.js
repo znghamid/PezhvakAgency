354d8d2b93e12d60192a5dce06a8726eb5091657
Hamidreza Zangooie <hrzangoie@gmail.com>
2020-12-10 22:08:28 +0330

    add login, register, password reset and contact us pages.

--- assets/js/custom/Script.js
+++ assets/js/custom/Script.js
@@ -1,6 +1,7 @@
 // Preloader page
 function loader(){
-    document.getElementById('preloader').classList.add('d-none');
+    $('#preloader').addClass('d-none');
+    // document.getElementById('preloader').classList.add('d-none');
 }
 
 // Lazyload
@@ -280,4 +281,20 @@ try {
     }
 } catch (error) {
     // 
+}
+
+
+// Auto height textaria
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+            
+function autosize(){
+    var el = this;
+    setTimeout(function(){
+        el.style.cssText = 'height:auto; padding:0';
+        // for box-sizing other than "content-box" use:
+        // el.style.cssText = '-moz-box-sizing:content-box';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    },0);
 }
